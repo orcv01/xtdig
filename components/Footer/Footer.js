00c49a74c2fb175699a7f033d676ad1b0239610b
UsuarioM <orcv01@gmail.com>
2023-09-08 14:07:37 +0200

    Update xtdis

---
+++ components/Footer/Footer.js
@@ -0,0 +1,55 @@
+import "./Footer.css";
+
+export const Footer = () => `
+<section class="foot">
+<div class="fot_1">
+<p>C/Fray Luis de León, 11<\p>
+<p>28012 Madrid<\p>
+<p>Tlf Centralita: 91 506 88 88<\p>
+<p>Tlf DAE: 91 506 89 18 / 19<\p>
+<p>Email: cursos.portalentodigital@fundaciononce.es<\p>
+</div>
+
+<div class="fot_2">
+<ul>
+<li>
+<a href="#" id="quelink">¿Qué es XTD?</a>
+</li>
+<li>
+<a href="#" id="cursoslink">Cursos</a>
+</li>
+<li>
+<a href="#" id="becaslink">Becas</a>
+</li>
+<li>
+<a href="#" id="bloglink">Blog</a>
+</li>
+<li>
+<a href="#" id="alumlink">Alumni</a>
+</li>
+<li>
+<a href="#" id="collink">Colaboradores</a>
+</li><li>
+<a href="#" id="acclink">Accesibilidad</a>
+</li><li>
+<a href="#" id="avisolink">Aviso legal</a>
+</li><li>
+<a href="#" id="polilink">Política de privacidad</a>
+</li><li>
+<a href="#" id="contlink">Contacto</a>
+</li><li>
+<a href="#" id="cooklink">Política de cookies</a>
+</li>
+</ul>
+<img class="xta" src="/icons/generaciond.png" alt="Generación España Digital"/>
+<img class="xta" src="/icons/icon-facebook.png" alt="Facebook"/>
+<img class="xta" src="/icons/icon-twitter.png" alt="Twitter"/>
+<img class="xta" src="/icons/icon-youtube.png" alt="Youtube"/>
+<img class="xta" src="/icons/icon-instagram.png" alt="Instagram"/>
+<img class="xta" src="/icons/icon-linkedin.png" alt="Linkedin"/>
+
+</div>
+
+<\section>
+
+`;
---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+import "./Navbar.css";
+
+import { Button } from "../Button/Button";
+
+export const Navbar = () => `
+
+<div>
+<img class="xta" src="imagenes/logoxtalento.png" alt="Fundación ONCE/Xtalento digital"/>
+<img class="xta" src="/icons/icon-facebook.png" alt="Facebook"/>
+<img class="xta" src="/icons/icon-twitter.png" alt="Twitter"/>
+<img class="xta" src="/icons/icon-youtube.png" alt="Youtube"/>
+<img class="xta" src="/icons/icon-instagram.png" alt="Instagram"/>
+<img class="xta" src="/icons/icon-linkedin.png" alt="Linkedin"/>
+<img class="xta" src="/icons/logo-gso2021.png" alt="Grupo Social ONCE"/>
+<\div>
+
+<nav>
+<ul>
+<li>
+<a href="#" id="quelink">¿Qué es XTD?</a>
+</li>
+<li>
+<a href="#" id="cursoslink">Cursos</a>
+</li>
+<li>
+<a href="#" id="becaslink">Becas</a>
+</li>
+<li>
+<a href="#" id="bloglink">Blog</a>
+</li>
+<li>
+<a href="#" id="alumlink">Alumni</a>
+</li>
+<li>
+<a href="#" id="collink">Colaboradores</a>
+</li>
+</ul>
+</nav>
+`;
